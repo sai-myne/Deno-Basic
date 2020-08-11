3f136aa0ce8dd49fe70ebe4e4ab1c4e72678c09c
sai-myne <ahlusir@gmail.com>
2020-08-11 13:51:36 +0800

    3rd Party Module

--- sandbox.ts
+++ sandbox.ts
@@ -1,36 +1,21 @@
-// // uuid module
-// import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+// import { serve } from "https://deno.land/std/http/server.ts";
 
-// const uid = v4.generate();
-// console.log(uid);
+// const server = serve({ port: 3000 });
+// console.log('listening for requests on port 3000');
 
-// // fs module
-// import { readJson, writeJson } from 'https://deno.land/std/fs/mod.ts';
+// for await (const req of server){
+//     console.log('request made');
+//     const url = req.url;
+//     req.respond({ body: `Hello again, ninjas, you visited ${url}`});
+// }
 
-// const jsonObj = await readJson('ninjas.json');
-// console.log(jsonObj);
+import { camelCase, paramCase, pascalCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
-// const books = [
-//     {
-//         "title": "the way of kings",
-//         "author": "brandon sanderson"
-//     },
-//     {
-//         "title": "name of the wind",
-//         "author": "patrick rothfuss"
-//     }
-// ]
+const text = 'hello again ninjas';
 
-// await writeJson('books.json', books, { spaces: 2 });
-// console.log('created books.json');
+const camel = camelCase(text);
+const param = paramCase(text);
+const pascal = pascalCase(text);
+const snake = snakeCase(text);
 
-// http module (server)
-import { serve } from 'https://deno.land/std/http/server.ts';
-
-const server = serve({ port: 3000 });
-console.log('listening for requests on port 3000');
-
-for await (const req of server){
-    const url = req.url;
-    req.respond({ body: `Hello ninjas, you visited ${url}`});
-}
+console.log(camel, param, pascal, snake);
